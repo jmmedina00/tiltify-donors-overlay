29f4acc3a62d3f5161dd2b5d451ee04821d24202
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2021-01-25 13:25:45 +0100

    feat(app): fetch data and decide what component is needed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Overlay from "./overlay/Overlay"
+import LinkGenerator from './link-generator/LinkGenerator';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const params = (new URL(document.location)).searchParams;
+
+  const accessToken = params.get("token");
+  const campaignId = params.get("campaign");
+
+  return accessToken && campaignId ? 
+  <Overlay token={accessToken} campaignId={campaignId}/> : 
+  <LinkGenerator/>;
 }
 
 export default App;
