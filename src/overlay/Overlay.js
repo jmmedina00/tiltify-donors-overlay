59fd7fa3f93f60dfeb22ad78bb7918df4d7baf64
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2021-01-26 12:47:14 +0100

    refactor(overlay): convert base component into a class and fit donations observable inside it

--- src/overlay/Overlay.js
+++ src/overlay/Overlay.js
@@ -1,32 +1,36 @@
-import { useEffect, useState } from "react";
 import { ajax } from "rxjs/ajax";
 import { interval } from "rxjs";
 import { map, switchMap, tap } from "rxjs/operators";
+import React from "react";
 
-const createObservable = (token, campaignId) => interval(5000).pipe(
-  switchMap(() => ajax({
-    url: `https://tiltify.com/api/v3/campaigns/${campaignId}/donations?count=100`,
-    method: "GET",
-    headers: {Authorization: `Bearer ${token}`}
-  })),
-  map(({response: { data }}) => data.map(({amount, name}) => ({amount, name}))),
-);
-
-let donations$;
-
-const Overlay = ({token, campaignId}) => {
-  if (!donations$) {
-    donations$ = createObservable(token, campaignId);
-  }
+class Overlay extends React.Component {
+  subscription = null;
   
-  const [donations, setDonations] = useState([]);
+  donations$ = interval(5000).pipe(
+    switchMap(() => ajax({
+      url: `https://tiltify.com/api/v3/campaigns/${this.props.campaignId}/donations?count=100`,
+      method: "GET",
+      headers: {Authorization: `Bearer ${this.props.token}`}
+    })),
+    map(({response: { data }}) => data.map(({amount, name}) => ({amount, name}))),
+    tap(data => console.log(data))
+  );
 
-  useEffect(() => {
-    const subscription = donations$.subscribe(setDonations);
-    return () => subscription.unsubscribe();
-  }, [])
+  updateDonations(donations) {
+    this.setState({donations});
+  }
 
-  return <p>{token} - {campaignId}</p>;
+  componentDidMount() {
+    this.subscription = this.donations$.subscribe(this.updateDonations.bind(this));
+  }
+
+  componentWillUnmount() {
+    this.subscription.unsubscribe();
+  }
+
+  render() {
+    return <p>{this.props.token} - {this.props.campaignId}</p>;
+  }
 }
 
 export default Overlay;
