3d4691935adc21b7f5ca09480d74fcbef4d43b95
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2021-01-26 12:59:38 +0100

    fix(overlay): hopefully prevent any access tokens from being banned

--- src/overlay/Overlay.js
+++ src/overlay/Overlay.js
@@ -1,16 +1,19 @@
 import { ajax } from "rxjs/ajax";
-import { interval } from "rxjs";
+import { timer } from "rxjs";
 import { map, switchMap, tap } from "rxjs/operators";
 import React from "react";
 
 class Overlay extends React.Component {
   subscription = null;
   
-  donations$ = interval(5000).pipe(
+  donations$ = timer(2000, 60000).pipe(
     switchMap(() => ajax({
       url: `https://tiltify.com/api/v3/campaigns/${this.props.campaignId}/donations?count=100`,
       method: "GET",
-      headers: {Authorization: `Bearer ${this.props.token}`}
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+        "User-Agent": "jmmedina00/donors-overlay"
+      }
     })),
     map(({response: { data }}) => data.map(({amount, name}) => ({amount, name}))),
     tap(data => console.log(data))
