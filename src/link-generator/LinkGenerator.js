b64b876e64a4d556f60c749c42fa320ba6610789
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2021-02-01 14:10:30 +0100

    chore(link-generator): arrange all elements visually

--- src/link-generator/LinkGenerator.js
+++ src/link-generator/LinkGenerator.js
@@ -2,8 +2,27 @@ const LinkGenerator = () =>
 <div className="vertical-center">
   <div className="horizontal-center">
     <div className="generator">
-      <p>Test: <input type="text"></input></p>
+      <div>
+        <label for="campaign">Campaign id</label>
+        <input type="text" id="campaign" name="campaign"></input>
+      </div>
+      <div>
+        <label for="token">Access token</label>
+        <input type="text" id="token" name="token"></input>
+      </div>
+      <div>
+        <span className="title">Currency</span>
+        <input type="radio" id="dollar" name="currency" value="dollar"/>
+          <label for="dollar">$</label>
+          <input type="radio" id="euro" name="currency" value="euro"/>
+          <label for="euro">€</label>
+      </div>
+      <div>
+        <label for="swap">Currency to the left?</label>
+        <input type="checkbox" id="swap" name="swap" checked="true"/>
+      </div>
     </div>
+    <button className="generate-link">Link</button>
   </div>
 </div>
 
