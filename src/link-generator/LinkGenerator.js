9f1849a1cdf7c10a77a0b820c8fb111a11fe1318
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2021-02-01 13:11:18 +0100

    refactor(link-generator): lay the foundation for the visual arrangement

--- src/link-generator/LinkGenerator.js
+++ src/link-generator/LinkGenerator.js
@@ -1,4 +1,10 @@
 const LinkGenerator = () => 
-<p>Test: <input type="text"></input></p>
+<div className="vertical-center">
+  <div className="horizontal-center">
+    <div className="generator">
+      <p>Test: <input type="text"></input></p>
+    </div>
+  </div>
+</div>
 
 export default LinkGenerator;
