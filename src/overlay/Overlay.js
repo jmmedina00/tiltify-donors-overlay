ff0babd75b6ce70e5991a91be5c3e55a151eee8c
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2021-02-02 13:04:53 +0100

    refactor(overlay): turn the component back to functional, create the observable in the selector

--- src/overlay/Overlay.js
+++ src/overlay/Overlay.js
@@ -2,58 +2,48 @@ import { connect } from "react-redux";
 import { ajax } from "rxjs/ajax";
 import { interval, timer } from "rxjs";
 import { map, switchMap, tap } from "rxjs/operators";
-import React from "react";
+import { useEffect, useState } from "react";
 import Donation from "./Donation";
 
-class Overlay extends React.Component {
-  subscription = null;
+const Overlay = ({donations$}) => {
+  const [state, setState] = useState({donations: []});
 
-  state = {donations: []};
+  useEffect(() => {
+    const subscription = donations$.subscribe(
+      donations => setState({donations}),
+      err => setState({error: true})
+    );
+
+    return () => {subscription.unsubscribe();};
+  }, [donations$]) // Don't resubscribe unless the donations$ observable itself changes
+ 
+  if (state.error) {
+    return <p>Token has been banned.</p>
+  }
   
-  donations$ = this.props.testMode ? 
+  const visualDonations = state.donations.map(
+    ({id, amount, name}) => <Donation key={id} amount={amount} name={name}/>
+  )
+  return <div>{visualDonations}</div>;
+}
+
+const mapStateToProps = ({token, campaignId, testMode}) => ({
+  donations$: testMode ? 
   interval(5000).pipe(
     map(count => new Array(count + 1).fill(0).map((_, index) => ({id: index, name: "Testing", amount: index * 0.5}))),
-    tap(data => console.log(data))
+    tap(data => console.log(data)),
   ) : 
   timer(2000, 60000).pipe(
     switchMap(() => ajax({
-      url: `https://tiltify.com/api/v3/campaigns/${this.props.campaignId}/donations?count=100`,
+      url: `https://tiltify.com/api/v3/campaigns/${campaignId}/donations?count=100`,
       method: "GET",
       headers: {
-        Authorization: `Bearer ${this.props.token}`,
-        "User-Agent": "jmmedina00/donors-overlay"
+        Authorization: `Bearer ${token}`
       }
     })),
     map(({response: { data }}) => data.map(({id, amount, name}) => ({id, amount, name}))),
     tap(data => console.log(data))
-  );
+  )
+});
 
-  updateDonations(donations) {
-    this.setState({donations});
-  }
-
-  componentDidMount() {
-    this.subscription = this.donations$.subscribe(
-      res => this.updateDonations(res),
-      err => this.setState({error: true})
-    );
-  }
-
-  componentWillUnmount() {
-    this.subscription.unsubscribe();
-  }
-
-  render() {
-    if (this.state.error) {
-      return <p>Token has been banned.</p>
-    }
-    
-    const visualDonations = this.state.donations.map(
-      ({id, amount, name}) => <Donation key={id} amount={amount} name={name}/>
-    )
-    return <div>{visualDonations}</div>;
-  }
-}
-
-const mapStateToProps = ({token, campaignId, testMode}) => ({token, campaignId, testMode});
 export default connect(mapStateToProps)(Overlay);
