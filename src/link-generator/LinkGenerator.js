b6d64633c96a08dbd4bc983c8063645ffcf0ff87
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2021-02-02 11:45:50 +0100

    refactor(link-generator): wrap all inputs in a form

--- src/link-generator/LinkGenerator.js
+++ src/link-generator/LinkGenerator.js
@@ -1,28 +1,35 @@
+const generateLink = event => {
+  event.preventDefault();
+  console.log(event);
+}
+
 const LinkGenerator = () => 
 <div className="vertical-center">
   <div className="horizontal-center">
-    <div className="generator">
-      <div>
-        <label for="campaign">Campaign id</label>
-        <input type="text" id="campaign" name="campaign"></input>
-      </div>
-      <div>
-        <label for="token">Access token</label>
-        <input type="text" id="token" name="token"></input>
-      </div>
-      <div>
-        <span className="title">Currency</span>
-        <input type="radio" id="dollar" name="currency" value="dollar"/>
-          <label for="dollar">$</label>
+    <form onSubmit={generateLink}>
+      <div className="generator">
+        <div>
+          <label htmlFor="campaign">Campaign id</label>
+          <input type="text" id="campaign" name="campaign"></input>
+        </div>
+        <div>
+          <label htmlFor="token">Access token</label>
+          <input type="text" id="token" name="token"></input>
+        </div>
+        <div>
+          <span className="title">Currency</span>
+          <input type="radio" id="dollar" name="currency" value="dollar"/>
+          <label htmlFor="dollar">$</label>
           <input type="radio" id="euro" name="currency" value="euro"/>
-          <label for="euro">€</label>
+          <label htmlFor="euro">€</label>
+        </div>
+        <div>
+          <label htmlFor="swap">Currency to the left?</label>
+          <input type="checkbox" id="swap" name="swap" defaultChecked="true"/>
+        </div>
       </div>
-      <div>
-        <label for="swap">Currency to the left?</label>
-        <input type="checkbox" id="swap" name="swap" checked="true"/>
-      </div>
-    </div>
-    <button className="generate-link">Link</button>
+      <button className="generate-link">Link</button>
+    </form>
   </div>
 </div>
 
