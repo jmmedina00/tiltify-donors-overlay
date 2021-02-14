535056647acf27937f09f0775d2afe758922400a
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2021-02-14 13:37:58 +0100

    test(app): write proper unit tests for this component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,59 @@
 import { render, screen } from './test-utils';
 import App from './App';
+import configureStore from 'redux-mock-store';
+import { loadParams } from './actions';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+jest.mock("./overlay/Overlay");
+jest.mock("./link-generator/LinkGenerator");
+
+describe("App", () => {
+  test("renders LinkGenerator by default", () => {
+    render(<App />);
+    expect(screen.getByText("This is link generator.")).toBeInTheDocument(); 
+  });
+
+  test("loads every single query parameter into the store", () => {
+    const store = configureStore([])({});
+    store.dispatch = jest.fn();
+
+    const data = {a: "1", b: "2"};
+    const mockParams = new URLSearchParams(data);
+
+    global.URL = function() { // Arrow functions don't work when mocking classes
+      return {
+        searchParams: mockParams
+      }
+    };
+
+    render(<App />, {initialState: {}, store});
+    expect(store.dispatch).toHaveBeenCalledWith(loadParams(data));
+  });
+
+  test("renders Overlay if the query parameters token and campaign are set", () => {
+    const data = {token: "abcd", campaign: "1234"};
+    const mockParams = new URLSearchParams(data);
+
+    global.URL = function() { // Arrow functions don't work when mocking classes
+      return {
+        searchParams: mockParams
+      }
+    };
+
+    render(<App />);
+    expect(screen.getByText("This is overlay.")).toBeInTheDocument();
+  });
+
+  test("renders LinkGenerator if either token and campaign parameters are missing", () => {
+    const data = {token: "abcd"};
+    const mockParams = new URLSearchParams(data);
+
+    global.URL = function() { // Arrow functions don't work when mocking classes
+      return {
+        searchParams: mockParams
+      }
+    };
+
+    render(<App />);
+    expect(screen.getByText("This is link generator.")).toBeInTheDocument();
+  });
+})
