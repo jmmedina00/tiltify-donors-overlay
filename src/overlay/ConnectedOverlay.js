480ee3bbf488fa3cb3f55bbd9e53c71a456717e6
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2021-02-17 13:25:41 +0100

    chore(connected-overlay): remove debug taps from resulting observables

--- src/overlay/ConnectedOverlay.js
+++ src/overlay/ConnectedOverlay.js
@@ -2,29 +2,25 @@ import Overlay from './Overlay';
 import { getDonations } from '../services/tiltify';
 import { connect } from 'react-redux';
 import { from, interval, timer } from 'rxjs';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 
 const mapStateToProps = ({ token, campaign, test, highest }) => ({
   donations$: test
     ? interval(5000).pipe(
         map(count =>
-          new Array(count + 1)
-            .fill(0)
-            .map((_, index) => ({
-              id: index,
-              name: 'Testing',
-              amount: index * 0.5,
-            }))
-        ),
-        tap(data => console.log(data))
+          new Array(count + 1).fill(0).map((_, index) => ({
+            id: index,
+            name: 'Testing',
+            amount: index * 0.5,
+          }))
+        )
       )
     : timer(2000, 60000).pipe(
         switchMap(() => from(getDonations(token, campaign))),
         map(data => data.map(({ id, amount, name }) => ({ id, amount, name }))),
         map(data =>
           !highest ? data : data.sort(({ amount: a }, { amount: b }) => b - a)
-        ),
-        tap(data => console.log(data))
+        )
       ),
 });
 
