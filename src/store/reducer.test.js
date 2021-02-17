df29a5f227432790ec40455b2ecd664b9a764d49
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2021-02-17 12:58:27 +0100

    test(reducer): add some basic unit tests

---
+++ src/store/reducer.test.js
@@ -0,0 +1,26 @@
+import { LOAD_PARAMS } from "./actions";
+import { reducer } from "./reducer";
+
+describe("Reducer", () => {
+  test("loads params upon receiving a LOAD_PARAMS action", () => {
+    const params = {a: "1", b: "2"};
+    expect(reducer({}, {type: LOAD_PARAMS, data: params}))
+    .toEqual(params);
+  });
+
+  test("fuses existing state with new params", () => {
+    const initialState = {a: "1", b: "2"};
+    const params = {b: "3", c: "4"};
+    const expected = {...initialState, ...params};
+    expect(reducer(initialState, {type: LOAD_PARAMS, data: params}))
+    .toEqual(expected);
+  });
+
+  test("returns state as is if anything else is received", () => {
+    const initialState = {a: "1", b: "2"};
+    const foreignAction = {
+      type: "IDONTKNOWWHATIMDOING", text: "Lorem ipsum"
+    };
+    expect(reducer(initialState, foreignAction)).toEqual(initialState);
+  })
+})
